0300e069845740beda12e6353a3833de5ff6abec
sly545 <sylvainferrandes@gmail.com>
2022-06-03 15:46:11 +0200

    changement et ajout du bouton pour remonter en haut de la page

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 let toggle = document.querySelector('.toggle');
 let body = document.querySelector('body');
+const fleche = document.querySelector('.fa-arrow-up');
+console.log(fleche);
 
+window.addEventListener('scroll',()=>{
+ if(window.scrollY> 500){
+     fleche.classList.add('scroll');
+ }
+ else{
+     fleche.classList.remove('scroll');
+ }
+    
+});
 toggle.addEventListener('click', function() {
     body.classList.toggle('open');
+})
+fleche.addEventListener('click',() =>{
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    })
 })
