b70fb5b4265890c641f0df5e368b9cd5b4386c71
sly545 <sylvainferrandes@gmail.com>
2023-06-02 11:16:22 +0200

    reglage bouton pour remonter

--- app.js
+++ app.js
@@ -3,28 +3,29 @@ let body = document.querySelector('body');
 const fleche = document.querySelector('.fa-arrow-up');
 const flecheversbas = document.querySelector('.fa-arrow-down');
 
-window.addEventListener('scroll',()=>{
- if(window.scrollY> 500){
-     fleche.classList.add('scroll');
- }
- else{
-     fleche.classList.remove('scroll');
- }
-    
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 500) {
+    fleche.classList.add('scroll');
+  } else {
+    fleche.classList.remove('scroll');
+  }
 });
-toggle.addEventListener('click', function() {
-    body.classList.toggle('open');
-})
-fleche.addEventListener('click',() =>{
-    window.scrollTo({
-        top:0,
-        left:0,
-        behavior: "smooth"
-    })
-})
-flecheversbas.addEventListener('click',() =>{
-    window.scrollTo({
-        top:650,
-        behavior: "smooth"
-    })
-})
+
+toggle.addEventListener('click', () => {
+  body.classList.toggle('open');
+});
+
+fleche.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+});
+
+flecheversbas.addEventListener('click', () => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth'
+  });
+});
